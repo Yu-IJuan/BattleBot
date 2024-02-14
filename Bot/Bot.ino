2b4cc336eb7dc8538dbd4b93691ac3bc2c72cf2c
Yu-I Juan <yu-i.juan@student.nhlstenden.com>
2024-02-14 05:35:54 +0100

    Updated pinouts

--- Bot/Bot.ino
+++ Bot/Bot.ino
@@ -3,26 +3,22 @@
 
 //PIN (Digital)
 const int Mot_A1    = 9,  Mot_A2  = 6;  //Right Motor (PWM)
-const int Mot_B1    = 5,  Mot_B2  = 11;  //Left Motor (PWM)
+const int Mot_B1    = 5,  Mot_B2  = 11; //Left Motor (PWM)
 const int Mot_R1    = 3,  Mot_R2  = 2;  //RPM Sensor
 const int trig      = 12, echo    = 13; //Ultrasonic
-const int Neo       = 7;                //NeoPixels
-const int Grab      = 4;               //Claw
+const int Grab      = 4;                //Claw
 const int ServoMot  = 10;               //Neck Servo
 //
 
 //PIN (Analog)
-const int D1 = A0, D2 = A1, D3 = A2, D4 = A3;
-const int D5 = A4, D6 = A5, D7 = A6, D8 = A7; //For IR B&W Sensor 
+const int D1 = A0, D3 = A1, D4 = A2;
+const int D5 = A3, D6 = A6, D8 = A7; //For IR B&W Sensor 
 //
 
 const int diskslot = 20;
 const float = 6.52;
-
-const int LED_COUNT = 4;                //NeoPixel's LED count
-Adafruit_NeoPixel strip(LED_COUNT, Neo, NEO_GRB + NEO_KHZ800);
                                         //NeoPixel's LED init setting
-DHT11 dht11(2);                         //DHT's PIN
+DHT11 dht11(1);                         //DHT's PIN
 int       temp;                         //For recording Temp
 
 unsigned long currenttime;              //For millis() functions
