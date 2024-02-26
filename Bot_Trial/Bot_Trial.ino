4a4edff3901e0cda0dc5b2ce1c9b71192f3238b2
Sofroniebruh <sofronie.albu1@student.nhlstenden.com>
2024-02-26 14:25:20 +0100

    changed the movement logic

--- Bot_Trial/Bot_Trial.ino
+++ Bot_Trial/Bot_Trial.ino
@@ -3,27 +3,26 @@
 #include <Wire.h>
 
 //PIN (Digital)
-const int Mot_A1 = 9, Mot_A2 = 6;   //Right Motor (PWM)
-const int Mot_B1 = 5, Mot_B2 = 11;  //Left Motor (PWM)
-const int Mot_R1 = 3, Mot_R2 = 2;   //RPM Sensor
-const int trig = 12, echo;          //Ultrasonic
-const int Grab = 4;                 //Claw
-const int ServoMot = 10;            //Neck Servo
+const int Mot_A1 = 9, Mot_A2 = 6;           //Right Motor (PWM)
+const int Mot_B1 = 5, Mot_B2 = 11;          //Left Motor (PWM)
+const int Mot_R1 = 3, Mot_R2 = 2;           //RPM Sensor
+const int trig = 12, echo;                  //Ultrasonic
+const int Grab = 4;                         //Claw
+const int ServoMot = 10;                    //Neck Servo
 //7,8,13 Reserved for ultrasonic sensors' echo pins
 //
 
 //PIN (Analog)
 const int D1 = A0, D3 = A1, D4 = A2;
-const int D5 = A3, D6 = A6, D8 = A7;  //For IR B&W Sensor
+const int D5 = A3, D6 = A6, D8 = A7;       //For IR B&W Sensor
 //
 
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/SCL, /* data=*/SDA, /* reset=*/U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 DHT11 dht11(1);  //DHT's PIN
 int temp;        //For recording Temp
 
-bool stateR = false, stateL = false;
-unsigned long currenttime, refreshtime;  //For millis() functions
-int duration, distance;                  //For Ultrasonic functions
+unsigned long currenttime, refreshtime;   //For millis() functions
+int duration, distance;         //For Ultrasonic functions
 unsigned int counterR, counterL;
 
 int distanceR, distanceL, distanceF;
@@ -70,34 +69,28 @@ void ISR_L() {
 }
 
 void loop() {
-  ultrasonic(13);
-  distanceR = distance;
-  ultrasonic(8);  //Checking with different ultrasonic sensors,
-  distanceF = distance;
-  ultrasonic(7);         //But they all share same trigger pin,
-  distanceL = distance;  //So it saves pin resources.
-  Serial.print("DistanceR:");
-  Serial.println(distanceR);
-  Serial.print("DistanceF:");
-  Serial.println(distanceF);
-  Serial.print("DiatanceL:");
-  Serial.println(distanceL);
-
+  if (millis() > currenttime) {
+    Serial.println("=========TIMED CHECK=========");
+    currenttime = millis() + 500;
+    ultrasonic(13);
+    distanceR = distance;
+    ultrasonic(8);                      //Checking with different ultrasonic sensors,
+    distanceF = distance;
+    ultrasonic(7);                      //But they all share same trigger pin,
+    distanceL = distance;               //So it saves pin resources.
+    Serial.print("DistanceR:");
+    Serial.println(distanceR);
+    Serial.print("DistanceF:");
+    Serial.println(distanceF);
+    Serial.print("DiatanceL:");
+    Serial.println(distanceL);
+    Serial.println("=========END Of TIMED CHECK==========");
+  }
   if (millis() >= refreshtime) {
     refreshtime = millis() + 1000;
     u8g2.clearBuffer();
-    if (stateL) {
-      u8g2.drawFrame(3, 0, 2, 62);
-      u8g2.drawBox(6, 0, 2, 62);
-      u8g2.drawBox(126, 0, 2, 62);
-    } else if (stateR) {
-      u8g2.drawFrame(123, 0, 2, 62);
-      u8g2.drawBox(120, 0, 2, 62);
-      u8g2.drawBox(0, 0, 2, 62);
-    } else {
-      u8g2.drawBox(123, 0, 2, 62);
-      u8g2.drawBox(3, 0, 2, 62);
-    }
+    u8g2.drawBox( 123, 0, 2, 62);
+    u8g2.drawBox( 3, 0, 2, 62);
     u8g2.setCursor(15, 10);
     u8g2.print("Front:");
     u8g2.print(distanceF);
@@ -112,9 +105,6 @@ void loop() {
   while (distanceF > 25 && distanceL < 25 && distanceR < 25) {
     u8g2.setCursor(34, 57);
     u8g2.print("Forward");
-
-    stateR = false;
-    stateL = false;
     Serial.println("Forward");
     forward(24, 24);                  //In center then walk straight
 
@@ -130,76 +120,43 @@ void loop() {
     Serial.println("Left");
     u8g2.setCursor(100, 15);
     u8g2.print("L");
-    u8g2.setFont(u8g2_font_squeezed_r6_tr);
-    u8g2.setCursor(96, 25);
-    u8g2.print("Turn");
-    u8g2.setFont(u8g2_font_8x13O_tr);
-    if (distanceF > 20) {
-      forward(5, 5);
-      delay(1000);
-    } else {
-      forward(2, 2);
-      delay(1000);
-    }
-    left(90);  //90 Degrees turn if left empty
-    delay(1000);
-    forward(23, 23);
-    delay(1000);
-  } else if (distanceF > 40) {  //Forward if front empty
-    u8g2.setCursor(34, 57);
-    u8g2.print("Forward");
-    if (distanceL + distanceR <= 70) {  //Check if in dual wall, calibrate purpose.
-      float distadjL = distanceL / 10;
-      int distroundL = distadjL;        //Bascially just comparing L/R distance in 2nd digit of CM,
-      float distadjR = distanceR / 10;  //For e.g. R:1xCM and L:2xCM, both divided by 10 then rounded to just 1 and 2,
-      int distroundR = distadjR;        //The numbers were gotten with making a float into int file type.
-      if (distroundR > distroundL) {
-        stateL = true;
-        Serial.println("R>L");
-        forward(abs(map(distroundL - distroundR, 1, 3, 24, 20)), 25);
-        delay(1000);
-      } else if (distroundR < distroundL) {
-        stateR = true;
-        Serial.println("L>R");
-        forward(25, abs(map(distroundL - distroundR, 1, 3, 24, 20)));  //Make calibration to the direction
-        delay(1000);
-      } else {
-        stateR = false;
-        stateL = false;
-        Serial.println("Forward");
-        forward(25, 25);  //In center then walk straight
-        delay(1000);
-      }
-    } else {
-      stateR = false;
-      stateL = false;
-      Serial.println("Forward");
-      forward(25, 25);  //In center then walk straight
-      delay(1000);
-    }
-  } else if (distanceR > 40) {
+    u8g2.sendBuffer();
+    left();                             //90 Degrees turn if left empty
+    forward(24, 24);
+    delay(300);
+  }
+  else if (distanceL < 25 && distanceF < 25 && distanceR > 25) {
     Serial.println("Right");
     u8g2.setCursor(100, 15);
     u8g2.print("R");
-    u8g2.setFont(u8g2_font_squeezed_r6_tr);
-    u8g2.setCursor(96, 25);
-    u8g2.print("Turn");
-    u8g2.setFont(u8g2_font_8x13O_tr);
-    right(90);  //90 Degrees turn if right empty
-    forward(23, 23);
-    delay(1000);
-  } else {
-    Serial.println("U-Turn");
+    u8g2.sendBuffer();
+    right();                            //90 Degrees turn if right empty
+    forward(24, 24);
+    delay(300);
+  }
+  else if (distanceL < 25 && distanceR < 25 && distanceF < 25) {
     u8g2.setCursor(100, 15);
-    u8g2.print("U");
-    u8g2.setFont(u8g2_font_squeezed_r6_tr);
-    u8g2.setCursor(96, 25);
-    u8g2.print("Turn");
-    u8g2.setFont(u8g2_font_8x13O_tr);
-    uturn();  //U-Turn if no route available
+    u8g2.print("B");
+    u8g2.sendBuffer();
+    while (distanceR < 25) {
+      Serial.println("Backward");
+      backward(24);                            //Backward if no route available
+      delay(1000);
+      ultrasonic(13);
+      distanceR = distance;
+    }
+    u8g2.setCursor(100, 15);
+    u8g2.print("R");
+    u8g2.sendBuffer();
+    right();
+    delay(1000);
+    forward(24, 24);
     delay(1000);
   }
-  delay(1000);
+  else if (distanceL < 25 && distanceR > 25 && distanceF > 25) {
+    Serial.println("Forward");
+    forward(24, 24);
+  }
 }
 
 
@@ -211,9 +168,8 @@ void ultrasonic(int echo) {
   digitalWrite(trig, LOW);
   duration = pulseIn(echo, HIGH);
   distance = duration * (0.0331 + 0.0006 * temp) / 2;
-  distance = abs(distance);
   if (distance >= 400) {
-    distance = 1;  //Maximum effective range of HC-SR04 is 400CM
+    distance = 400;                   //Maximum effective range of HC-SR04 is 400CM
   }
 }
 
@@ -247,39 +203,8 @@ void forward(int stepsR, int stepsL) {
   counterR = 0;
 }
 
-void backward(int steps) {
-  counterL = 0;
-  counterR = 0;
-  while (steps > counterR && steps > counterL) {
-    if (steps > counterR) {
-      analogWrite(Mot_A1, 255);
-      analogWrite(Mot_A2, 0);
-    } else {
-      analogWrite(Mot_A1, 0);
-      analogWrite(Mot_A2, 0);
-    }
-    if (steps > counterL) {
-      analogWrite(Mot_B1, 0);
-      analogWrite(Mot_B2, 250);
-    } else {
-      analogWrite(Mot_B1, 0);
-      analogWrite(Mot_B2, 0);
-    }
-    Serial.println(counterL);
-    Serial.println(counterR);
-  }
-  analogWrite(Mot_A1, 0);
-  analogWrite(Mot_A2, 0);
-  analogWrite(Mot_B1, 0);
-  analogWrite(Mot_B2, 0);
-  counterL = 0;
-  counterR = 0;
-}
-
-void left(int angle) {
-  int steps;
-  if (angle == 90) steps = 8;
-  else steps = 4;
+void left() {
+  int steps = 8;
   counterL = 0;
   counterR = 0;
   u8g2.setCursor(30, 55);
@@ -287,7 +212,7 @@ void left(int angle) {
   u8g2.setCursor(90, 55);
   u8g2.print(steps);
   u8g2.sendBuffer();
-  while (steps - 1 > counterR || steps > counterL) {
+  while (steps > counterR || steps > counterL) {
     if (steps > counterR) {
       analogWrite(Mot_A1, 0);
       analogWrite(Mot_A2, 225);
@@ -311,10 +236,8 @@ void left(int angle) {
   counterR = 0;
 }
 
-void right(int angle) {
-  int steps;
-  if (angle == 90) steps = 8;
-  else steps = 4;
+void right() {
+  int steps = 8;
   counterL = 0;
   counterR = 0;
   u8g2.setCursor(30, 55);
@@ -346,32 +269,21 @@ void right(int angle) {
   counterR = 0;
 }
 
-void uturn() {
-  right(4);
-  delay(1000);
-  backward(6);
-  delay(1000);
-  left(4);
-  delay(1000);
-  int steps = 16;
+
+void backward(int steps) {
   counterL = 0;
   counterR = 0;
-  u8g2.setCursor(30, 55);
-  u8g2.print("Steps:");
-  u8g2.setCursor(90, 55);
-  u8g2.print(steps);
-  u8g2.sendBuffer();
   while (steps > counterR || steps > counterL) {
     if (steps > counterR) {
-      analogWrite(Mot_A1, 220);
+      analogWrite(Mot_A1, 255);
       analogWrite(Mot_A2, 0);
     } else {
       analogWrite(Mot_A1, 0);
       analogWrite(Mot_A2, 0);
     }
     if (steps > counterL) {
-      analogWrite(Mot_B1, 255);
-      analogWrite(Mot_B2, 0);
+      analogWrite(Mot_B1, 0);
+      analogWrite(Mot_B2, 250);
     } else {
       analogWrite(Mot_B1, 0);
       analogWrite(Mot_B2, 0);
@@ -383,5 +295,4 @@ void uturn() {
   analogWrite(Mot_B2, 0);
   counterL = 0;
   counterR = 0;
-  forward(15, 15);
 }
