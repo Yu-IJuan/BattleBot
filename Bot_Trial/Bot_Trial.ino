0386ff607be5303b0b473ac44a823e86d66c8dad
Yu-IJuan <yu-i.juan@student.nhlstenden.com>
2024-02-27 23:40:22 +0100

    Simpified Logic & Delete duplicated code

--- Bot_Trial/Bot_Trial.ino
+++ Bot_Trial/Bot_Trial.ino
@@ -3,26 +3,28 @@
 #include <Wire.h>
 
 //PIN (Digital)
-const int Mot_A1 = 9, Mot_A2 = 6;           //Right Motor (PWM)
-const int Mot_B1 = 5, Mot_B2 = 11;          //Left Motor (PWM)
-const int Mot_R1 = 3, Mot_R2 = 2;           //RPM Sensor
-const int trig = 12, echo;                  //Ultrasonic
-const int Grab = 4;                         //Claw
-const int ServoMot = 10;                    //Neck Servo
+const int Mot_A1 = 9, Mot_A2 = 6;   //Right Motor (PWM)
+const int Mot_B1 = 5, Mot_B2 = 11;  //Left Motor (PWM)
+const int Mot_R1 = 3, Mot_R2 = 2;   //RPM Sensor
+const int trig = 12, echo;          //Ultrasonic
+const int Grab = 4;                 //Claw
+const int ServoMot = 10;            //Neck Servo
 //7,8,13 Reserved for ultrasonic sensors' echo pins
 //
 
+const int MagicNumber = 40, MagicDelay = 300;
+
 //PIN (Analog)
 const int D1 = A0, D3 = A1, D4 = A2;
-const int D5 = A3, D6 = A6, D8 = A7;       //For IR B&W Sensor
+const int D5 = A3, D6 = A6, D8 = A7;  //For IR B&W Sensor
 //
 
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/SCL, /* data=*/SDA, /* reset=*/U8X8_PIN_NONE);
 DHT11 dht11(1);  //DHT's PIN
 int temp;        //For recording Temp
 
-unsigned long currenttime, refreshtime;   //For millis() functions
-int duration, distance;         //For Ultrasonic functions
+unsigned long currenttime, refreshtime;  //For millis() functions
+int duration, distance;                  //For Ultrasonic functions
 unsigned int counterR, counterL;
 
 int distanceR, distanceL, distanceF;
@@ -53,9 +55,7 @@ void setup() {
   temp = dht11.readTemperature();
   u8g2.begin();
   u8g2.setFont(u8g2_font_8x13O_tr);
-  delay(1000);
-  Serial.begin(9600);
-  Serial.println("--------------Setup complete!----------------");
+  delay(MagicDelay);
 }
 
 void ISR_R() {
@@ -68,27 +68,19 @@ void ISR_L() {
 
 void loop() {
   if (millis() > currenttime) {
-    Serial.println("=========TIMED CHECK=========");
     currenttime = millis() + 500;
     ultrasonic(13);
     distanceR = distance;
-    ultrasonic(8);                      //Checking with different ultrasonic sensors,
+    ultrasonic(8);  //Checking with different ultrasonic sensors,
     distanceF = distance;
-    ultrasonic(7);                      //But they all share same trigger pin,
-    distanceL = distance;               //So it saves pin resources.
-    Serial.print("DistanceR:");
-    Serial.println(distanceR);
-    Serial.print("DistanceF:");
-    Serial.println(distanceF);
-    Serial.print("DiatanceL:");
-    Serial.println(distanceL);
-    Serial.println("=========END Of TIMED CHECK==========");
+    ultrasonic(7);         //But they all share same trigger pin,
+    distanceL = distance;  //So it saves pin resources.
   }
   if (millis() >= refreshtime) {
-    refreshtime = millis() + 1000;
+    refreshtime = millis() + MagicDelay;
     u8g2.clearBuffer();
-    u8g2.drawBox( 123, 0, 2, 62);
-    u8g2.drawBox( 3, 0, 2, 62);
+    u8g2.drawBox(123, 0, 2, 62);
+    u8g2.drawBox(3, 0, 2, 62);
     u8g2.setCursor(15, 10);
     u8g2.print("Front:");
     u8g2.print(distanceF);
@@ -100,91 +92,67 @@ void loop() {
     u8g2.print(distanceL);
     u8g2.sendBuffer();
   }
-  while (distanceF > 25 && distanceL < 25 && distanceR < 25) {
-    u8g2.setCursor(34, 57);
-    u8g2.print("Forward");
-    Serial.println("Forward");
-    forward(24, 24);                  //In center then walk straight
-
-    ultrasonic(13);
-    distanceR = distance;
-    ultrasonic(8);                      //Checking with different ultrasonic sensors,
-    distanceF = distance;
-    ultrasonic(7);                      //But they all share same trigger pin,
-    distanceL = distance;               //So it saves pin resources.
-    delay(1000);
-  }
-  if (distanceL > 25 && distanceF > 25 && distanceR > 25 || distanceL > 25 && distanceF < 25 && distanceR > 25 || distanceL > 25 && distanceF > 25 || distanceL > 25 && distanceF < 25 && distanceR < 25) {
-    Serial.println("Left");
+  if (distanceL >= MagicNumber) {
     u8g2.setCursor(100, 15);
     u8g2.print("L");
     u8g2.sendBuffer();
-    left();                             //90 Degrees turn if left empty
+    forward(12,12);
+    left();  //90 Degrees turn if left empty
     forward(24, 24);
-    delay(300);
-  }
-  else if (distanceL < 25 && distanceF < 25 && distanceR > 25) {
-    Serial.println("Right");
+    delay(MagicDelay);
+  } else if (distanceF >= MagicNumber) {
+    u8g2.setCursor(10, 57);
+    u8g2.print("Const-Forward");
+    constforward();  //In center then walk straight
+    delay(MagicDelay);
+  } else if (distanceR >= MagicNumber) {
     u8g2.setCursor(100, 15);
     u8g2.print("R");
     u8g2.sendBuffer();
-    right();                            //90 Degrees turn if right empty
+    right();  //90 Degrees turn if right empty
     forward(24, 24);
-    delay(300);
-  }
-  else if (distanceL < 25 && distanceR < 25 && distanceF < 25) {
+    delay(MagicDelay);
+  } else {
     u8g2.setCursor(100, 15);
     u8g2.print("B");
     u8g2.sendBuffer();
-    while (distanceR < 25 && distanceL < 25) {
-      Serial.println("Backward");
-      backward(24);                            //Backward if no route available
-      delay(1000);
+    while (distanceR <= MagicNumber && distanceL <= MagicNumber) {
+      u8g2.setCursor(30, 57);
+      u8g2.print("Backward");
+      backward(24);  //Backward if no route available
+      delay(MagicDelay);
       ultrasonic(13);
       distanceR = distance;
       ultrasonic(7);
       distanceL = distance;
     }
-    if (distanceL > 25) {
+    if (distanceR >= MagicNumber) {
+      u8g2.setCursor(100, 15);
+      u8g2.print("R");
+      u8g2.sendBuffer();
+      right();
+      delay(MagicDelay);
+      forward(24, 24);
+      delay(MagicDelay);
+    } else if (distanceL >= MagicNumber) {
       u8g2.setCursor(100, 15);
       u8g2.print("L");
       u8g2.sendBuffer();
       left();
-      delay(1000);
-      if (distanceL > 25 && distanceF > 25) {
+      delay(MagicDelay);
+      if (distanceL >= MagicNumber) {
         u8g2.setCursor(100, 15);
         u8g2.print("L");
         u8g2.sendBuffer();
         left();
-        delay(1000);
-      }
-      else if (distanceL < 25 && distanceF > 25) {
+        delay(MagicDelay);
         forward(24, 24);
-        delay(1000);
+        delay(MagicDelay);
+      } else {
+        forward(24, 24);
+        delay(MagicDelay);
       }
     }
-    else if (distanceR > 25) {
-      u8g2.setCursor(100, 15);
-      u8g2.print("R");
-      u8g2.sendBuffer();
-      right();
-      delay(1000);
-      forward(24, 24);
-      delay(1000);
-    }
-    else if (distanceR > 25 && distanceL > 25) {
-      u8g2.setCursor(100, 15);
-      u8g2.print("R");
-      u8g2.sendBuffer();
-      right();
-      delay(1000);
-      forward(24, 24);
-      delay(1000);
-    }
-  }
-  else if (distanceL < 25 && distanceR > 25 && distanceF > 25) {
-    Serial.println("Forward");
-    forward(24, 24);
   }
 }
 
@@ -197,9 +165,100 @@ void ultrasonic(int echo) {
   digitalWrite(trig, LOW);
   duration = pulseIn(echo, HIGH);
   distance = duration * (0.0331 + 0.0006 * temp) / 2;
-  if (distance >= 400) {
-    distance = 400;                   //Maximum effective range of HC-SR04 is 400CM
+  if (distance >= 400) distance = 1;  //Maximum effective range of HC-SR04 is 400CM
+}
+
+void constforward() {
+  attachInterrupt(digitalPinToInterrupt(Mot_R1), ISR_R, RISING);
+  attachInterrupt(digitalPinToInterrupt(Mot_R2), ISR_L, RISING);
+  int steps;
+  counterL = 0;
+  counterR = 0;
+  steps = 4;
+  int Mot_AlaR = 254;
+  int Mot_AlaL = 250;
+  while (distanceF >= 20) {
+    u8g2.clearBuffer();
+    if (steps > counterR || steps > counterL) {
+      int offset = distanceL - distanceR;
+      if (offset < 7 && offset >= 3) {
+        Mot_AlaR = 255;
+        Mot_AlaL = 245;
+        u8g2.setCursor(30, 30);
+        u8g2.print("SmolL");
+        delay(500);
+        Mot_AlaR = 254;
+        Mot_AlaL = 250;
+        counterL = counterR = 0;
+      } else if (offset >= 7) {
+        Mot_AlaR = 255;
+        Mot_AlaL = 240;
+        u8g2.setCursor(30, 30);
+        u8g2.print("BigL");
+        delay(750);
+        Mot_AlaR = 254;
+        Mot_AlaL = 250;
+        counterL = counterR = 0;
+      } else if (-7 < offset && offset <= -3) {
+        Mot_AlaR = 245;
+        Mot_AlaL = 255;
+        u8g2.setCursor(30, 30);
+        u8g2.print("SmolR");
+        delay(500);
+        Mot_AlaR = 254;
+        Mot_AlaL = 250;
+        counterL = counterR = 0;
+      } else if (offset <= -7) {
+        Mot_AlaR = 240;
+        Mot_AlaL = 255;
+        u8g2.setCursor(30, 30);
+        u8g2.print("BigR");
+        delay(750);
+        Mot_AlaR = 254;
+        Mot_AlaL = 250;
+        counterL = counterR = 0;
+      } else {
+        Mot_AlaR = 254;
+        Mot_AlaL = 250;
+        u8g2.setCursor(96, 30);
+        u8g2.print(offset);
+        counterL = counterR = 0;
+      }
+      if (steps > counterR) {
+        analogWrite(Mot_A1, 0);
+        analogWrite(Mot_A2, Mot_AlaR);
+      } else {
+        analogWrite(Mot_A1, 0);
+        analogWrite(Mot_A2, 0);
+      }
+      if (steps > counterL) {
+        analogWrite(Mot_B1, Mot_AlaL);
+        analogWrite(Mot_B2, 0);
+      } else {
+        analogWrite(Mot_B1, 0);
+        analogWrite(Mot_B2, 0);
+      }
+      u8g2.setCursor(96, 30);
+      u8g2.print(offset);
+      u8g2.sendBuffer();
+    }
+    steps = steps + 4;
+    ultrasonic(8);
+    distanceF = distance;
+    ultrasonic(7);
+    distanceL = distance;
+    ultrasonic(13);
+    distanceR = distance;
+    if (distanceL >= MagicNumber) break;
   }
+  analogWrite(Mot_A1, 0);
+  analogWrite(Mot_A2, 0);
+  analogWrite(Mot_B1, 0);
+  analogWrite(Mot_B2, 0);
+  counterL = 0;
+  counterR = 0;
+  detachInterrupt(digitalPinToInterrupt(Mot_R1));
+  detachInterrupt(digitalPinToInterrupt(Mot_R2));
 }
 
 void forward(int stepsR, int stepsL) {
@@ -211,7 +270,7 @@ void forward(int stepsR, int stepsL) {
   while (stepsR > counterR || stepsL > counterL) {
     if (stepsR > counterR) {
       analogWrite(Mot_A1, 0);
-      analogWrite(Mot_A2, 255);
+      analogWrite(Mot_A2, 254);
     } else {
       analogWrite(Mot_A1, 0);
       analogWrite(Mot_A2, 0);
@@ -223,8 +282,6 @@ void forward(int stepsR, int stepsL) {
       analogWrite(Mot_B1, 0);
       analogWrite(Mot_B2, 0);
     }
-    Serial.println(counterL);
-    Serial.println(counterR);
   }
   detachInterrupt(digitalPinToInterrupt(Mot_R1));
   detachInterrupt(digitalPinToInterrupt(Mot_R2));
@@ -318,7 +375,7 @@ void backward(int steps) {
   attachInterrupt(digitalPinToInterrupt(Mot_R2), ISR_L, RISING);
   while (steps > counterR || steps > counterL) {
     if (steps > counterR) {
-      analogWrite(Mot_A1, 255);
+      analogWrite(Mot_A1, 254);
       analogWrite(Mot_A2, 0);
     } else {
       analogWrite(Mot_A1, 0);
