bf60babc8f5d810964c473a036f4ef7090200401
Yu-IJuan <yu-i.juan@student.nhlstenden.com>
2024-03-06 15:32:35 +0100

    Init Upload

---
+++ Bot_Trial_LED/Bot_Trial_LED.ino
@@ -0,0 +1,457 @@
+#include <DHT11.h>
+#include <Adafruit_NeoPixel.h>
+
+//PIN (Digital)
+const int Mot_A1 = 9, Mot_A2 = 6;   //Right Motor (PWM)
+const int Mot_B1 = 5, Mot_B2 = 11;  //Left Motor (PWM)
+const int Mot_R1 = 3, Mot_R2 = 2;   //RPM Sensor
+const int trig, echo;               //Ultrasonic
+const int Grab = 4;                 //Claw
+const int LED = 10;                 //LED pin
+//7 ,8 ,13 Reserved for ultrasonic sensors' echo pins
+//10, A4, A5 Reserved for ultrasonic sensors' trig pins
+
+
+const int MagicNumber = 35, MagicDelay = 300;
+const int PinCount = 4;
+
+//PIN (Analog)
+const int D1 = A0, D3 = A1, D4 = A2;
+const int D5 = A3, D6 = A6, D8 = A7;  //For IR B&W Sensor
+//
+
+DHT11 dht11(1);  //DHT's PIN
+int temp;        //For recording Temp
+
+Adafruit_NeoPixel pixels(PinCount, LED, NEO_GRB + NEO_KHZ800);
+
+unsigned long currenttime, refreshtime;  //For millis() functions
+int duration, distance;                  //For Ultrasonic functions
+unsigned int counterR, counterL;
+
+int distanceR, distanceL, distanceF;
+int pulseWidth;
+
+bool stateServo = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(Mot_A1, OUTPUT);
+  pinMode(Mot_A2, OUTPUT);
+  pinMode(Mot_B1, OUTPUT);
+  pinMode(Mot_B2, OUTPUT);
+
+  pinMode(D1, INPUT);
+  pinMode(D3, INPUT);
+  pinMode(D4, INPUT);
+  pinMode(D5, INPUT);
+  pinMode(D6, INPUT);
+  pinMode(D8, INPUT);
+
+  pinMode(10, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
+  pinMode(13, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+
+  pinMode(Grab, OUTPUT);
+  //  for (int i; i < 50; i++) moving(20);
+  //  for (int i; i < 50; i++) moving(70);
+  temp = dht11.readTemperature();
+  pixels.begin(); 
+}
+
+void moving(int angle) {
+  int pulseWidth = map(angle, 0, 180, 544, 2400);
+
+  for (int i; i < 10; i++) {
+    digitalWrite(Grab, HIGH);
+    delayMicroseconds(pulseWidth);
+    digitalWrite(Grab, LOW);
+    delayMicroseconds(20000 - pulseWidth);
+  }
+}
+
+void ISR_R() {
+  counterR++;
+}
+
+void ISR_L() {
+  counterL++;
+}
+
+void loop() {
+  if (!stateServo) {
+    moving(90);
+    delay(200);
+    stateServo = true;
+  }
+  moving(30);
+
+
+  if (millis() >= currenttime) {
+    currenttime = millis() + MagicNumber;
+    ultrasonic(13);
+    distanceR = distance;
+    ultrasonic(8);  //Checking with different ultrasonic sensors,
+    distanceF = distance;
+    ultrasonic(7);         //But they all share same trigger pin,
+    distanceL = distance;  //So it saves pin resources.
+  }
+  if (millis() >= refreshtime) {
+    refreshtime = millis() + MagicDelay;
+    LED("Default");
+  }
+  if (distanceR >= MagicNumber) {
+    LED("R");
+    turncalibrate("forward", "right");
+    delay(MagicDelay);
+    right();  //90 Degrees turn if left empty
+    LED("F");
+    delay(MagicDelay);
+    forward(24, 24);
+    delay(MagicDelay);
+  } else if (distanceF >= MagicNumber) {
+    LED("F");
+    constforward();  //In center then walk straight
+    delay(MagicDelay);
+  } else if (distanceL >= MagicNumber) {
+    LED("L");
+    turncalibrate("forward", "left");
+    delay(MagicDelay);
+    left();  //90 Degrees turn if right empty
+    LED("F");
+    delay(MagicDelay);
+    forward(24, 24);
+    delay(MagicDelay);
+  } else {
+    LED("B");
+    constBackward();  //Backward if no route available
+    delay(MagicDelay);
+    ultrasonic(7);
+    distanceL = distance;
+    ultrasonic(13);
+    distanceR = distance;
+    if (distanceL >= MagicNumber) {
+      LED("L");
+      turncalibrate("backward", "default");
+      delay(MagicDelay);
+      left();
+      delay(MagicDelay);
+      LED("F");
+      forward(24, 24);
+      delay(MagicDelay);
+    } else if (distanceR >= MagicNumber) {
+      LED("R");
+      right();
+      delay(MagicDelay);
+      ultrasonic(13);
+      distanceR = distance;
+      ultrasonic(8);
+      distanceF = distance;
+      if (distanceR >= MagicNumber) {
+        LED("R");
+        right();
+        delay(MagicDelay);
+        LED("F");
+        forward(24, 24);
+        delay(MagicDelay);
+      } else if (distanceF >= MagicNumber) {
+        LED("F");
+        forward(24, 24);
+        delay(MagicDelay);
+      }
+    }
+  }
+}
+
+
+void ultrasonic(int echo) {
+  if (echo == 7) trig = A5;
+  else if (echo == 8) trig = 12;
+  else trig = A4;
+  digitalWrite(trig, LOW);
+  delay(5);
+  digitalWrite(trig, HIGH);
+  delay(10);
+  digitalWrite(trig, LOW);
+  duration = pulseIn(echo, HIGH);
+  delay(50);
+  distance = duration * (0.0331 + 0.0006 * temp) / 2;
+  if (distance >= 400) distance = 1;  //Maximum effective range of HC-SR04 is 400CM
+}
+
+void LED(String dir) {
+  if (dir == "L")
+    else if (dir == "R") else if (dir == "B") else if (dir == "F")
+      moving(30);
+}
+
+void constforward() {
+  attachInterrupt(digitalPinToInterrupt(Mot_R1), ISR_R, RISING);
+  attachInterrupt(digitalPinToInterrupt(Mot_R2), ISR_L, RISING);
+  int Mot_AlaR = 245;
+  int Mot_AlaL = 250;
+  LED("F");
+  int stepsL = 4;
+  int stepsR = 4;
+  counterL = 0;
+  counterR = 0;
+  while (distanceF >= MagicNumber) {
+    while (stepsR - 1 > counterR || stepsL > counterL) {
+      if (stepsR - 1 > counterR) {
+        analogWrite(Mot_A1, 0);
+        analogWrite(Mot_A2, 246);
+      } else {
+        analogWrite(Mot_A1, 0);
+        analogWrite(Mot_A2, 0);
+      }
+      if (stepsL > counterL) {
+        analogWrite(Mot_B1, 250);
+        analogWrite(Mot_B2, 0);
+      } else {
+        analogWrite(Mot_B1, 0);
+        analogWrite(Mot_B2, 0);
+      }
+      ultrasonic(8);
+      distanceF = distance;
+      ultrasonic(13);
+      distanceR = distance;
+    }
+    stepsL = stepsL + 4;
+    stepsR = stepsR + 4;
+
+    if (distanceR >= MagicNumber) break;
+    else if (distanceF <= MagicNumber) break;
+  }
+  analogWrite(Mot_A1, 0);
+  analogWrite(Mot_A2, 0);
+  analogWrite(Mot_B1, 0);
+  analogWrite(Mot_B2, 0);
+  detachInterrupt(digitalPinToInterrupt(Mot_R1));
+  detachInterrupt(digitalPinToInterrupt(Mot_R2));
+}
+
+void constBackward() {
+  int stepsR, stepsL;
+  stepsR = stepsL = 2;
+  counterL = 0;
+  counterR = 0;
+  attachInterrupt(digitalPinToInterrupt(Mot_R1), ISR_R, RISING);
+  attachInterrupt(digitalPinToInterrupt(Mot_R2), ISR_L, RISING);
+  while (distanceL <= MagicNumber || distanceR <= MagicNumber) {
+    ultrasonic(7);
+    distanceL = distance;
+    ultrasonic(13);
+    distanceR = distance;
+    if (distanceL >= MagicNumber) break;
+    else if (distanceR >= MagicNumber) break;
+    while (stepsR > counterR || stepsL > counterL) {
+      if (stepsR > counterR) {
+        analogWrite(Mot_A1, 245);
+        analogWrite(Mot_A2, 0);
+      } else {
+        analogWrite(Mot_A1, 0);
+        analogWrite(Mot_A2, 0);
+      }
+      if (stepsL > counterL) {
+        analogWrite(Mot_B1, 0);
+        analogWrite(Mot_B2, 250);
+      } else {
+        analogWrite(Mot_B1, 0);
+        analogWrite(Mot_B2, 0);
+      }
+    }
+    stepsL = stepsL + 2;
+    stepsR = stepsR + 2;
+  }
+  analogWrite(Mot_A1, 0);
+  analogWrite(Mot_A2, 0);
+  analogWrite(Mot_B1, 0);
+  analogWrite(Mot_B2, 0);
+  detachInterrupt(digitalPinToInterrupt(Mot_R1));
+  detachInterrupt(digitalPinToInterrupt(Mot_R2));
+}
+
+void forward(int stepsR, int stepsL) {
+  counterL = 0;
+  counterR = 0;
+  attachInterrupt(digitalPinToInterrupt(Mot_R1), ISR_R, RISING);
+  attachInterrupt(digitalPinToInterrupt(Mot_R2), ISR_L, RISING);
+  while (stepsR > counterR || stepsL > counterL) {
+    if (stepsR > counterR) {
+      analogWrite(Mot_A1, 0);
+      analogWrite(Mot_A2, 245);
+    } else {
+      analogWrite(Mot_A1, 0);
+      analogWrite(Mot_A2, 0);
+    }
+    if (stepsL > counterL) {
+      analogWrite(Mot_B1, 250);
+      analogWrite(Mot_B2, 0);
+    } else {
+      analogWrite(Mot_B1, 0);
+      analogWrite(Mot_B2, 0);
+    }
+  }
+  detachInterrupt(digitalPinToInterrupt(Mot_R1));
+  detachInterrupt(digitalPinToInterrupt(Mot_R2));
+  analogWrite(Mot_A1, 0);
+  analogWrite(Mot_A2, 0);
+  analogWrite(Mot_B1, 0);
+  analogWrite(Mot_B2, 0);
+  counterL = 0;
+  counterR = 0;
+}
+
+void backward(int stepsR, int stepsL) {
+  counterL = 0;
+  counterR = 0;
+  attachInterrupt(digitalPinToInterrupt(Mot_R1), ISR_R, RISING);
+  attachInterrupt(digitalPinToInterrupt(Mot_R2), ISR_L, RISING);
+  while (stepsR - 1 > counterR || stepsL > counterL) {
+    if (stepsR - 1 > counterR) {
+      analogWrite(Mot_A1, 250);
+      analogWrite(Mot_A2, 0);
+    } else {
+      analogWrite(Mot_A1, 0);
+      analogWrite(Mot_A2, 0);
+    }
+    if (stepsL > counterL) {
+      analogWrite(Mot_B1, 0);
+      analogWrite(Mot_B2, 253);
+    } else {
+      analogWrite(Mot_B1, 0);
+      analogWrite(Mot_B2, 0);
+    }
+  }
+  detachInterrupt(digitalPinToInterrupt(Mot_R1));
+  detachInterrupt(digitalPinToInterrupt(Mot_R2));
+  analogWrite(Mot_A1, 0);
+  analogWrite(Mot_A2, 0);
+  analogWrite(Mot_B1, 0);
+  analogWrite(Mot_B2, 0);
+  counterL = 0;
+  counterR = 0;
+}
+
+void left() {
+  int steps = 8;
+  attachInterrupt(digitalPinToInterrupt(Mot_R1), ISR_R, RISING);
+  attachInterrupt(digitalPinToInterrupt(Mot_R2), ISR_L, RISING);
+  counterL = 0;
+  counterR = 0;
+  while (steps > counterR || steps > counterL) {
+    if (steps > counterR) {
+      analogWrite(Mot_A1, 0);
+      analogWrite(Mot_A2, 205);
+    } else {
+      analogWrite(Mot_A1, 0);
+      analogWrite(Mot_A2, 0);
+    }
+    if (steps > counterL) {
+      analogWrite(Mot_B1, 0);
+      analogWrite(Mot_B2, 205);
+    } else {
+      analogWrite(Mot_B1, 0);
+      analogWrite(Mot_B2, 0);
+    }
+  }
+  analogWrite(Mot_A1, 0);
+  analogWrite(Mot_A2, 205);
+  analogWrite(Mot_B1, 0);
+  analogWrite(Mot_B2, 205);
+  delay(41);
+  analogWrite(Mot_A1, 0);
+  analogWrite(Mot_A2, 0);
+  analogWrite(Mot_B1, 0);
+  analogWrite(Mot_B2, 0);
+  counterL = 0;
+  counterR = 0;
+  detachInterrupt(digitalPinToInterrupt(Mot_R1));
+  detachInterrupt(digitalPinToInterrupt(Mot_R2));
+}
+
+void right() {
+  int steps = 8;
+  attachInterrupt(digitalPinToInterrupt(Mot_R1), ISR_R, RISING);
+  attachInterrupt(digitalPinToInterrupt(Mot_R2), ISR_L, RISING);
+  counterL = 0;
+  counterR = 0;
+  while (steps - 1 > counterR || steps > counterL) {
+    if (steps - 1 > counterR) {
+      analogWrite(Mot_A1, 205);
+      analogWrite(Mot_A2, 0);
+    } else {
+      analogWrite(Mot_A1, 0);
+      analogWrite(Mot_A2, 0);
+    }
+    if (steps > counterL) {
+      analogWrite(Mot_B1, 205);
+      analogWrite(Mot_B2, 0);
+    } else {
+      analogWrite(Mot_B1, 0);
+      analogWrite(Mot_B2, 0);
+    }
+  }
+  analogWrite(Mot_A1, 205);
+  analogWrite(Mot_A2, 0);
+  analogWrite(Mot_B1, 205);
+  analogWrite(Mot_B2, 0);
+  delay(41);
+  analogWrite(Mot_A1, 0);
+  analogWrite(Mot_A2, 0);
+  analogWrite(Mot_B1, 0);
+  analogWrite(Mot_B2, 0);
+  counterL = 0;
+  counterR = 0;
+  detachInterrupt(digitalPinToInterrupt(Mot_R1));
+  detachInterrupt(digitalPinToInterrupt(Mot_R2));
+}
+
+void turncalibrate(String advancedir, String dir) {
+  if (advancedir == "forward") {
+    int Mot_AlaR = 246;
+    int Mot_AlaL = 250;
+    analogWrite(Mot_A1, Mot_AlaR);
+    analogWrite(Mot_A2, 0);
+    analogWrite(Mot_B1, 0);
+    analogWrite(Mot_B2, Mot_AlaL);
+    if (dir == "left") {
+      while (distanceL >= MagicNumber) {
+        ultrasonic(7);
+        distanceL = distance;
+      }
+    } else if (dir == "right") {
+      while (distanceR >= MagicNumber) {
+        ultrasonic(13);
+        distanceR = distance;
+      }
+    }
+    analogWrite(Mot_A1, 0);
+    analogWrite(Mot_A2, 0);
+    analogWrite(Mot_B1, 0);
+    analogWrite(Mot_B2, 0);
+    delay(MagicDelay);
+    forward(22, 22);
+  } else {
+    int Mot_AlaR = 250;
+    int Mot_AlaL = 253;
+    analogWrite(Mot_A1, 0);
+    analogWrite(Mot_A2, Mot_AlaR);
+    analogWrite(Mot_B1, Mot_AlaL);
+    analogWrite(Mot_B2, 0);
+    while (distanceL >= MagicNumber) {
+      ultrasonic(7);
+      distanceL = distance;
+    }
+    analogWrite(Mot_A1, 0);
+    analogWrite(Mot_A2, 0);
+    analogWrite(Mot_B1, 0);
+    analogWrite(Mot_B2, 0);
+    delay(MagicDelay);
+    backward(6, 6);
+    return;
+  }
+}
